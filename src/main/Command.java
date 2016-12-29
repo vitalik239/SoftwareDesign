4ea205604d3629f96505abd25f69c49d336b707c
Vitalik Plastinin <vitalik@Air-Vitalik.local>
2016-12-29 16:45:03 +0300

    master cleaned

--- src/main/Command.java
+++
@@ -1,9 +0,0 @@
-package main;
-
-import java.io.InputStream;
-import java.util.ArrayList;
-
-public interface Command {
-    public InputStream execute(ArrayList<String> args, InputStream prev)
-            throws CommandException;
-}
